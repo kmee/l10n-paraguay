223573b43e51ea08f9747bdfefb9c1d405310ec8
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2026-08-18 19:56:23 -0300

    [ADD] l10n_py_account_payment_bancard_qr: module scaffolding

---
+++ l10n_py_account_payment_bancard_qr/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Paraguay - Bancard QR SPI Payment Provider (Skeleton)",
+    "version": "18.0.1.0.0",
+    "category": "Accounting/Payment Providers",
+    "summary": "Honest skeleton for a future Bancard QR/Infonet (SPI contactless) "
+    "payment provider integration",
+    "author": "KMEE, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-paraguay",
+    "license": "LGPL-3",
+    "depends": [
+        "payment",
+    ],
+    "data": [
+        "views/payment_provider_views.xml",
+        "data/payment_provider_data.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
